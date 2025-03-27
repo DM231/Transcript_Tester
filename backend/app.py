8f0b83e5aefeaa7661d605cc6371af5b89697109
DIAA MUSTAFA <diaa.mostfa2002@gmail.com>
2025-03-27 05:49:34 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -759,4 +759,4 @@ def upload_file():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
