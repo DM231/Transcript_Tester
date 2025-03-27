682a6ed01bd1d65ba87d2ab7efcbd3361fbded8b
DIAA MUSTAFA <diaa.mostfa2002@gmail.com>
2025-03-27 05:48:26 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -758,4 +758,5 @@ def upload_file():
         return "Lütfen bir Word (.docx) dosyası yükleyin."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
